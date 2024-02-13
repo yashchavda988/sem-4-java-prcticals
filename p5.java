ab976c041e42a7e7bc34ff04c8fd78f1ceca78f4
Yash <129313358+yashchavda988@users.noreply.github.com>
2024-02-13 19:34:20 +0530

    Create p5.java

---
+++ p5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class p5 {
+    static boolean isPrime(int n) {
+        if (n == 1 || n == 0)
+            return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter N number :- ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
